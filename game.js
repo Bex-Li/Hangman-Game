0f2661601704e8a22c2fd87e1838a2051dbb73c2
Bex-Li <rebekkikko@gmail.com>
2018-03-15 18:43:43 -0700

    updated JS script tag in HTML

---
+++ game.js
@@ -0,0 +1,71 @@
+console.log("anything we want"); 
+// var wordArray = [
+//     "syntax",
+//     "jquery",
+//     "browser",
+//     "pseudo",
+//     "laptop",
+//     "variable",
+// ];
+
+//     // var guessesLeft += 8
+//     var winCounter 
+//     var loseCounter
+//     var wordToGuess 
+//     var lettersInWord
+//         numDashes
+//     var dashAndGoodGuesses
+//     var wrongGuesses
+
+// // Function getWord array
+// // guessesLeft = 9;
+// //then random choose
+// //then lettersInWord = wordToGuess.split
+
+// //var numDashes = lettersInWord.length
+// //array dashAndGoodGuesses to change the spaces _ to letters "a"
+
+// //for loop to put dashes
+// for ( ;; ) {
+//     console.log();
+// }
+// // <!-- var nameArray = ("syntax", "jquery", "browser", "pseudo", laptop) {
+// // 	forLoop ........	
+// // } -->
+
+
+// //getElementByID for guessesLeft
+// //getElementByID for wordToGuess
+// //getElementByID for wrongGuesses
+
+// //onKeyUp function? 
+// document.onkeyup = function(event) {
+//     // Converts all key clicks to lowercase letters.
+//     letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+//     console.log(letterGuessed);
+//     startgame(letterGuessed);
+//     roundComplete();
+//    };
+
+// //Function: getWord 
+
+// //Function: Start Game 
+//    //if/else
+
+// //Function: Round Complete 
+//     //win/lose
+//         //if
+//             // alert("Congrats! You WON!");
+//         //else
+//             // alert("Boo hoo, try again!")
+//     //update fields
+//         //if/else
+//         //getElement
+
+
+
+
+// // function myFunction() {
+// //     var x = document.getElementById("newWord");
+// //     x.value = x.value.toUpperCase();
+// // }
